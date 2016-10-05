dbdf78021791111e9543821c7797ff62247c9c55
ankitesh kushwaha <ankitesh.kushwana@naukri.com>
2016-10-05 14:54:17 -0300

    hacker rank folder

---
+++ exe/hackerRank/hackerRanck_Connected_Cells_in_a_Grid.cpp
@@ -0,0 +1,26 @@
+#include<isotream>
+#include<stdio.h>
+#include<stdlib>
+using namespace std;
+
+int highestNoIsland(int *arr , int r , int c){
+int visit[r][c];
+
+}
+
+
+
+
+int main(){
+
+int m,n;
+cin>>m>>n;
+int input[m][n], visit[m][n];
+for(int i=0 ; i<m ; i++){
+  for(int j=0 ; j<n ; j++)
+    cin>>arr[i][j];
+}
+cout<<highestNoIsland(arr ,m,n);
+
+}
+
